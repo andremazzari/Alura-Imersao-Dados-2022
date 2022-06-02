19efe8d8a0e1519209f35117d92050c12638a9df
André Saugo Mazzari <amazzari@ifi.unicamp.br>
2022-06-02 19:57:07 -0300

    'creating'

---
+++ README.md
@@ -0,0 +1,45 @@
+Alura-Imersao-Dados-2022
+==============================
+
+A short description of the project.
+
+Project Organization
+------------
+
+    ├── README.md          <- The top-level README for developers using this project.
+    ├── data
+    │   ├── external       <- Data from third party sources.
+    │   ├── interim        <- Intermediate data that has been transformed.
+    │   ├── processed      <- The final, canonical data sets for modeling.
+    │   └── raw            <- The original, immutable data dump.
+    │
+    ├── models             <- Trained and serialized models, model predictions, or model summaries
+    │
+    ├── notebooks          <- Jupyter notebooks.
+    │
+    ├── references         <- Data dictionaries, manuals, and all other explanatory materials.
+    │
+    ├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
+    │                         generated with `pip freeze > requirements.txt`
+    │
+    ├── src                <- Source code for use in this project.
+    │   ├── __init__.py    <- Makes src a Python module
+    │   │
+    │   ├── data           <- Scripts to download or generate data
+    │   │   └── make_dataset.py
+    │   │
+    │   ├── features       <- Scripts to turn raw data into features for modeling
+    │   │   └── build_features.py
+    │   │
+    │   ├── models         <- Scripts to train models and then use trained models to make
+    │   │   │                 predictions
+    │   │   ├── predict_model.py
+    │   │   └── train_model.py
+    │   │
+    │   └── visualization  <- Scripts to create exploratory and results oriented visualizations
+    │       └── visualize.py
+
+
+--------
+
+<p><small>Project based on the <a target="_blank" href="https://drivendata.github.io/cookiecutter-data-science/">cookiecutter data science project template</a>. #cookiecutterdatascience</small></p>
---
+++ src/data/make_dataset.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+def AddIBGEdata(df):
+    #Read data from enderecos.csv
+    df_address = pd.read_csv('../data/interim/Data_Day4_Address.csv', sep = ',', encoding = 'utf_8')
+
+    #Extract address data from collumn 'Rua', and create column 'Rua_Compare' to make merge with df_address
+    df["Rua_Compare"] = df["Rua"].str.extract(r'(^[\w ]+)')
+    df["Rua_Compare"] = df["Rua_Compare"].str.lower().str.strip()
+
+    #Merge df and df_address
+    df_merged = df.merge(df_address[['cep','latitude','longitude','Rua_Compare']], how = 'left', left_on = 'Rua_Compare', right_on = "Rua_Compare")
+
+    #Drop duplicates
+    df_merged.drop_duplicates(subset=df.columns, inplace = True)
+
+    #Get data from censo sectors  in Sao Paulo using geopandas
+    setor_censo = gpd.read_file('../data/raw/35SEE250GC_SIR.shp')
+    setor_censo_sp = setor_censo[setor_censo.NM_MUNICIP == "SÃO PAULO"]
+
+    df_merged["Point"] = ""
+    for i in df_merged.index:
+        df_merged["Point"][i] = Point(df_merged["longitude"][i], df_merged["latitude"][i])
+    df_merged['setor_censo'] = df_merged["Point"].map(lambda x: setor_censo_sp.loc[setor_censo_sp.contains(x), 'CD_GEOCODI'].values).str[0]
+
+    df_merged['setor_censo'] = pd.to_numeric(df_merged['setor_censo'])
+
+    #Get data from IBGE (already clenaed)
+    df_ibge = pd.read_csv('../data/interim/IBGE_cleaned.csv')
+
+    #Merge the original dataframe with IBGE dataframe
+    df_prices_ibge = pd.merge(left = df_merged, right = df_ibge, how = "left", left_on = "setor_censo", right_on = "Cod_setor")
+
+    #Drop rows with NaN value in 'setor_censo'
+    df_prices_ibge.drop(df_prices_ibge[df_prices_ibge['setor_censo'].isnull()].index.tolist(), axis = 0, inplace = True)
+
+    return df_prices_ibge[['Metragem', 'Quartos', 'Banheiros', 'Vagas', 'Valor', 'V005', 'V007', 'V009', 'V011']]
